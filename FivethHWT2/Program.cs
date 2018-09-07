f1200ae29c295c5449b6c07ad9ac00e02b713a5c
Nazar <nazar2131@ukr.net>
2018-09-07 15:17:32 +0300

    Add FivethHWT2

---
+++ FivethHWT2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FivethHWT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>(10);
+            Console.WriteLine("Capacity: {0}", list.Capacity);
+            for (int i = 0; i < list.Capacity; i++)
+            {
+                Console.Write($"enter num {i+1}: ");
+                list.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+            foreach(int s in list)
+            {
+                Console.WriteLine($" {s} = {s}");
+            }
+
+            Console.WriteLine($"Index of element -10 = { list.IndexOf(-10)}");
+            Console.ReadKey();
+        }
+    }
+}
